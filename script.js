951eb642d577d7d492e99795048ceb4621501b40
Shubhang Tripathi <124138495+Shubhangtripathi809@users.noreply.github.com>
2024-10-26 21:01:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,12 @@
 document.getElementById('loginForm').addEventListener('submit', function (event) {
-    event.preventDefault(); // Prevent form submission
+    event.preventDefault();
 
-    // Hardcoded credentials for demonstration
     const correctUsername = "user123";
     const correctPassword = "pass123";
 
-    // Get the input values
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
 
-    // Check if the credentials are correct
     if (username === correctUsername && password === correctPassword) {
         window.location.href = "dashboard.html"; // Redirect to the dashboard page
     } else {
